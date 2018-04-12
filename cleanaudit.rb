41c178cebed1cf40eaf3740c8754d14a237a6b73
ccaldarella99 <christopher.caldarella@outlook.com>
2018-04-12 11:13:38 -0600

    Original file I was given

---
+++ cleanaudit.rb
@@ -0,0 +1,174 @@
+
+exit if Object.const_defined?(:Ocra)
+
+def parse_transtime(line)
+	if line =~ /^(\d{1,2}:\d\d\s[AP]M)(,|\s)/
+		transtime = $1
+	end
+	transtime
+end
+
+def parse_transtype(line)
+	if line =~ /(,|\s)(\w+)\sPAYMENT/
+		transtype = $2
+	end
+	transtype
+end
+
+def parse_cardtype(line)
+	if (line =~ /(VISA|MC|M\/C|MASTERCARD|AMEX|DISC|DISCOVER)\son\s/i ||
+	    line =~ /\,(VISA|MC|M\/C|MASTERCARD|AMEX|DISC|DISCOVER)/i ||
+	    line =~ /-\s(VISA|MC|M\/C|MASTERCARD|AMEX|DISC|DISCOVER)/i)
+		cardtype = $1
+	end
+	cardtype
+end
+
+def parse_table(line)
+	if line =~ /\son\s(.+?)\s(Chk:)/
+		table = $1
+	end
+	table
+end
+
+def parse_check(line, transtype)
+	if transtype == 'MOVE'
+		if line =~ /\sChk:(\d{5,6})\sto\sChk:(\d{5,6})\s/
+			check = "#{$1} -> #{$2}"
+		end
+	else
+		if line =~ /\sChk:(\d{5,6})\s/
+			check = $1
+		end
+	end
+	check
+end
+
+def parse_employee(line)
+	if (line =~ /\sChk:\d{5,6}\sby\s(.+)\s-\s/ ||
+	    line =~ /PAYMENT,(Mgr:.+Emp:.+)\son\s/ ||
+	    line =~ /PAYMENT\s(Mgr:.+Emp:.+)\son\s/)
+		employee = $1
+	end
+	employee
+end
+
+def parse_authamt(line, transtype)
+	if (transtype == 'APPLY' || transtype == 'MOVE')
+		if line =~ /\s-\s,*(\d+\.\d\d)\s/
+			authamt = $1
+		end
+	elsif transtype == 'ADJUST'
+		if line =~ /\s-\s,*Amt:\d+\.\d\d\s->\s(\d+\.\d\d)\s/
+			authamt = $1
+		elsif line =~ /\s-\s,*Amt:(\d+\.\d\d)\s/
+			authamt = $1
+		end
+	elsif transtype == 'DELETE'
+		if line =~ /(\s|,)(\d+\.\d\d)\sTip:/
+			authamt = $2
+		end
+	end
+	authamt
+end
+
+def parse_startip(line)
+	if line =~ /Tip:(\d+\.\d\d)\s->\s\d+\.\d\d/
+		starttip = $1
+	end
+	starttip
+end
+
+def parse_endtip(line)
+	if line =~ /Tip:\d+\.\d\d\s->\s(\d+\.\d\d)/
+		endtip = $1
+	elsif line =~ /Tip:(\d+\.\d\d)\s/
+		endtip = $1
+	end
+	if endtip == '0.00'
+		endtip = ''
+	end
+	endtip
+end
+
+def parse_cardnum(line)
+	if line =~ /\sID:(\d{15,16})\s/
+		cardnum = $1
+	elsif line =~ /Tip:\d+\.\d\d\s(\d{15,16})\sExp:/
+		cardnum = $1
+	elsif line =~ /\sID:X+\d{4}\s->\s(\d{15,16})\sExp:/
+		cardnum = $1	
+	end
+	if (cardnum)
+		cardnum = "#{cardnum}_"
+	end
+	cardnum
+end
+
+def parse_expiration(line)
+	if line =~ /\sExp:(\d{4})/
+		expiration = $1
+	end
+	expiration
+end
+
+def parse_cardmask(line, cardnum)
+	if cardnum =~ /(\d{4})\_/
+		cardmask = $1
+	elsif line =~ /X{11,12}(\d{4})\s/
+		cardmask = $1
+	end
+	cardmask
+end
+
+
+
+
+Dir.glob("Audit????.csv") do |audit|
+	cleanfile = audit.gsub(/Audit/, "cleanaud")
+	cleanaudit = File.new(cleanfile, "w")
+	File.open(audit, "r") do |auditlines|
+		cleanaudit.puts "TIME,TRANSTYPE,CARDTYPE,TABLE,CHECK,EMPLOYEE,AUTHAMT,STARTTIP,ENDTIP,CARDNUM,EXP,CARDMASK"
+		line_number = 0
+		while line = auditlines.gets
+			if (line_number == 1 && !(line =~ /\d{4}\sExp:\d{4}/))
+				next
+			elsif line_number == 1
+				line_number = 2
+			end
+			if (line =~ /^\d{1,2}:\d\d\s[AP]M.+PAYMENT/ && line_number == 0)
+				line_number = 1
+			end
+			if (line_number == 1)
+				txn = line.chomp
+			elsif (line_number == 2)
+				txn = "#{txn} - #{line}"
+				
+				transtime = parse_transtime(txn)
+				transtype = parse_transtype(txn)
+				cardtype = parse_cardtype(txn)
+				table = parse_table(txn)
+				check = parse_check(txn, transtype)
+				employee = parse_employee(txn)
+				authamt = parse_authamt(txn, transtype)
+				starttip = parse_startip(txn)
+				endtip = parse_endtip(txn)
+				cardnum = parse_cardnum(txn)
+				expiration = parse_expiration(txn)
+				cardmask = parse_cardmask(txn, cardnum)
+			
+			
+				if (transtime && transtype && authamt && cardmask)
+					cleanaudit.puts "#{transtime},#{transtype},#{cardtype},#{table},#{check},#{employee},#{authamt},#{starttip},#{endtip},#{cardnum},#{expiration},#{cardmask}"
+				else
+					cleanaudit.puts "ERROR,\"#{txn}\",#{transtime},#{transtype},#{authamt},#{cardmask}"
+				end
+				
+				line_number = 0
+			end
+		end
+	end
+	cleanaudit.close
+end
+
+	
